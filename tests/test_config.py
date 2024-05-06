189ac21ed9798596312aba889ddb6e5cb295703a
Adrian Ruben Dogar <adrian.dogar@gmail.com>
2024-05-07 01:53:56 +0200

    Turn it into a python module

--- tests/test_config.py
+++ tests/test_config.py
@@ -19,8 +19,8 @@ class ConfigTest(unittest.TestCase):
     @patch('builtins.open', new_callable=mock_open, read_data='{"key": {"$ref": "SECRET_KEY"}}')
     @patch('os.getenv', return_value=None)
     def test_add_secrets_with_missing_env_var(self, mock_getenv, mock_file):
-        config = Config("config.json")
-        self.assertIsNone(config.key)
+        config = Config("config.json").items()
+        self.assertIsNone(config['key'])
 
     @patch('builtins.open', new_callable=mock_open, read_data='{"key": {"$ref": "SECRET_KEY", "required": true}}')
     @patch('os.getenv', return_value=None)
@@ -36,24 +36,24 @@ class ConfigTest(unittest.TestCase):
     @patch('builtins.open', new_callable=mock_open, read_data='{"key": {"$ref": "SECRET_KEY"}}')
     @patch('os.getenv', return_value="secret_value")
     def test_add_secrets_with_ref(self, mock_getenv, mock_file):
-        config = Config("config.json")
-        self.assertEqual(config.key, "secret_value")
+        config = Config("config.json").items()
+        self.assertEqual(config['key'], "secret_value")
 
     @patch('builtins.open', new_callable=mock_open, read_data='{"key1": "value1", "key2": "value2"}')
     def test_load_config_as_attributes(self, mock_file):
-        config = Config("config.json")
-        self.assertEqual(config.key1, "value1")
-        self.assertEqual(config.key2, "value2")
+        config = Config("config.json").items()
+        self.assertEqual(config['key1'], "value1")
+        self.assertEqual(config['key2'], "value2")
 
     @patch('builtins.open', new_callable=mock_open, read_data='{"key": "value"}')
     def test_to_string(self, mock_file):
         config = Config("config.json")
-        self.assertEqual(config.to_string(), "{'errors': [], 'object': 'config.json', 'original': {'key': 'value'}, 'key': 'value'}")
+        self.assertEqual(config.to_string(), "{'errors': [], 'object': 'config.json', 'original': {'key': 'value'}, 'completed': {'key': 'value'}}")
 
     @patch('builtins.open', new_callable=mock_open, read_data='{"key": "value"}')
     def test_to_json(self, mock_file):
         config = Config("config.json")
-        self.assertEqual(config.to_json(), '{\n  "errors": [],\n  "object": "config.json",\n  "original": {\n    "key": "value"\n  },\n  "key": "value"\n}')
+        self.assertEqual(config.to_json(), '{\n  "errors": [],\n  "object": "config.json",\n  "original": {\n    "key": "value"\n  },\n  "completed": {\n    "key": "value"\n  }\n}')
 
 
 if __name__ == '__main__':
